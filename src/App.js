3c635d85c9ef6cec8451a4f8b3de1fd801ec6043
zyz <zyzisgood@gmail.com>
2020-05-14 10:55:31 -0500

    ref update

--- src/App.js
+++ src/App.js
@@ -1,49 +1,58 @@
-import React ,{Component,useState,createContext,useContext}from 'react';
+import React ,{Component,useState,useMemo,useRef,useCallback,memo, PureComponent, useEffect}from 'react';
 
-const CountContext = createContext();
 
-class Foo extends Component{
+
+// const Counter = memo(function Counter(props){
+//   console.log('counter render');
+//   return (
+//   <h1 onClick={props.onClick}>{props.count}</h1>
+//   )
+//  }
+// )
+
+class Counter extends PureComponent{
   render(){
-    return(
-      <CountContext.Consumer>
-        {
-          count => <h1>{count}</h1>
-        }
-      </CountContext.Consumer>
+    const {props} = this;
+    return (
+    <h1 onClick={props.onClick}>{props.count}</h1>
     )
   }
-}
-
-class Bar extends Component{
-  static contextType = CountContext;
-  render(){
-    const count = this.context;
-    return(
-      <h1>{count}</h1>
-    )
-  }
-}
-
-function Counter(){
-  const count = useContext(CountContext);
-  return (
-  <h1>{count}</h1>
-  )
 }
 
 function App(props){
 
   const [count,setCount] = useState(0);
+  const counterRef = useRef();
+  const double = useMemo(()=>{
+    return count * 2;
+  },[count === 3]);
+
+  let it = useRef();
+  const onClick = useCallback(()=> {
+      console.log('Click');
+      console.log(counterRef.current);
+    },[counterRef]); 
+
+  useEffect(()=> {
+    it.current = setInterval(()=> {
+       setCount(count => count + 1);
+    },1000);
+  },[]);
+
+  useEffect(()=> {
+    if(count >= 10){
+       clearInterval(it.current);
+    }
+  });
+
 
   return (
     <div>
-      <button onClick={()=>setCount(count + 1)}>Click ({count})</button>
-      <CountContext.Provider value={count}>
-        <Foo></Foo>
-        <Bar></Bar>
-        <Counter></Counter>
-      </CountContext.Provider>
-     
+      <button onClick={()=>setCount(count + 1)}>
+        Click ({count})
+        Double ({double})
+        </button>
+        <Counter ref={counterRef} count={double} onClick={onClick}></Counter>    
     </div>
   )
 }

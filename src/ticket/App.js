5e266c8b7f44bafa7bfd4a967f34b8111e7423d3
zyz <zyzisgood@gmail.com>
2020-05-17 21:15:54 -0500

    update2

---
+++ src/ticket/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {connect} from 'react-redux';
+
+function App(props){
+
+
+}
+
+export default connect(
+    function mapStateToProps(state){},
+    function mapDispatchToProps(dispatch){}
+)(App);
